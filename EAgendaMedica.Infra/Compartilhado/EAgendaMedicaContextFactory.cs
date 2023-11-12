4c897dc4199f54d2660e3e058eb6e9dbc7595c22
Bruno G Carvalho <113460204+brunogomescarvalho@users.noreply.github.com>
2023-11-12 15:17:05 -0300

    Add Camada Infra, Contexto Banco de Dados

---
+++ EAgendaMedica.Infra/Compartilhado/EAgendaMedicaContextFactory.cs
@@ -0,0 +1,21 @@
+﻿namespace EAgendaMedica.Infra.Compartilhado
+{
+    public class EAgendaMedicaContextFactory : IDesignTimeDbContextFactory<EAgendaMedicaDBContext>
+    {
+        public EAgendaMedicaDBContext CreateDbContext(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+
+            string connectionString = configuration.GetConnectionString("SqlServer");
+
+            var optionsBuilder = new DbContextOptionsBuilder<EAgendaMedicaDBContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new EAgendaMedicaDBContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ EAgendaMedica.Infra/Compartilhado/EAgendaMedicaDBContext.cs
@@ -0,0 +1,33 @@
+﻿namespace EAgendaMedica.Infra.Compartilhado
+{
+    public class EAgendaMedicaDBContext : DbContext, IContextoPersistencia
+    {
+        public EAgendaMedicaDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public async Task SalvarDados()
+        {
+            await this.SaveChangesAsync();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.EnableSensitiveDataLogging();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Type tipo = typeof(EAgendaMedicaDBContext);
+
+            Assembly dllComConfiguracoesOrm = tipo.Assembly;
+
+            modelBuilder.ApplyConfigurationsFromAssembly(dllComConfiguracoesOrm);
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+
+
+}
